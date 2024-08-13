d554c3046615edf9f61212c7090d53c70ee5b839
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-08-12 19:56:50 -0600

    Creación de la interfaz Asignatura con sus métodos.

--- Examen/Alumno.cs
+++ Examen/Alumno.cs
@@ -3,7 +3,7 @@ namespace Examen
 {
     public class Alumno
     {
-        public string NombreAlumno { get; set; }
+        public  string NombreAlumno { get; set; }
         public string NumeroCuenta { get; set; }
         public string Email { get; set; }
     }
